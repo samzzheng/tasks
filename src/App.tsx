023a3ff42c5cd7c295243ec672a194e6a2eafeaf
samzzheng <samzheng@udel.edu>
2024-09-06 23:08:39 -0400

    Made Changes to HTML and CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -8,7 +9,33 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <h1>Samuel Zheng</h1>
-            <h2>Hello World</h2>
+            <header>This is my header</header>
+            <img
+                src="images/kekwait.jpg"
+                alt="Zoomed in picture of a mans face"
+            />
+
+            <ul>
+                <li>Thing 1</li>
+                <li>Thing 2</li>
+                <li>Thing 3</li>
+            </ul>
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <div style={{ background: "red", width: 100, height: 200 }}>
+                <Container>
+                    <Row>
+                        <Col></Col>
+                    </Row>
+                    <Col></Col>
+                </Container>
+            </div>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
