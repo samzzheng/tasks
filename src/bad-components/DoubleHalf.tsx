9b54c54f3da00ccd53d83fedb170bfc02dc8f687
samzzheng <samzheng@udel.edu>
2024-10-03 23:31:54 -0400

    trying to fix linting error

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -7,23 +7,40 @@ interface DoubleHalfProps {
 }
 
 function Doubler({ dhValue, setDhValue }: DoubleHalfProps): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+    return (
+        <Button
+            onClick={() => {
+                setDhValue(2 * dhValue);
+            }}
+        >
+            Double
+        </Button>
+    );
 }
 
 function Halver({ dhValue, setDhValue }: DoubleHalfProps): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+    return (
+        <Button
+            onClick={() => {
+                setDhValue(0.5 * dhValue);
+            }}
+        >
+            Halve
+        </Button>
+    );
 }
 
 export function DoubleHalf(): JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler dhValue={dhValue} setDhValue={setDhValue}></Doubler>
-            <Halver dhValue={dhValue} setDhValue={setDhValue}></Halver>
+            <Doubler dhValue={dhValue} setDhValue={setDhValue} />
+            <Halver dhValue={dhValue} setDhValue={setDhValue} />
         </div>
     );
 }
